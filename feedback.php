96f35517962f802a6692783c2f0c479bcf3fce32
Victor Pimentel <victor.pimentel@gmail.com>
2011-04-27 19:39:42 +0200

    URL change

--- feedback.php
+++ feedback.php
@@ -26,7 +26,7 @@ if (array_key_exists('feedback', $_REQUEST)) {
     $email = $_REQUEST['email'];
     $name = $_REQUEST['name'];
   } else {
-    $email = 'tvshows.help@gmail.com';
+    $email = 'support@tvshowsapp.com';
     $name = 'Unknown Submitter';
   }
   $feedback = $_REQUEST['feedback'];
@@ -43,7 +43,7 @@ if (array_key_exists('feedback', $_REQUEST)) {
   $msg .= "Bundle ID: $bundleID\n";
   $msg .= "System Profile: $systemProfile\n";
 
-  mail("tvshows.help@gmail.com", "[$feedbackType] $appName $appVersion", $msg, $headers);
+  mail("support@tvshowsapp.com", "[$feedbackType] $appName $appVersion", $msg, $headers);
 }
 ?>
 
